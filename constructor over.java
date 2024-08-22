69ec525c6d80a220668f63b7cf440c466f30aa9d
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2024-08-22 12:19:59 +0530

    Create constructor over.java

---
+++ constructor over.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class cons{
+    cons(){
+    System.out.println("hello sarath");
+    }
+    
+    void ss(int n){
+     System.out.println(n);
+     }
+     public static void main(String[]args){
+     cons obj = new cons();
+     obj.ss(10);
+     }
+     }
+    
