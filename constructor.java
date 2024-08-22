27443937a6aaf4e318455f84ab30d57e256b27f0
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2024-08-22 12:16:03 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class cons{
+    cons(){
+    System.out.println("hello sarath");
+    }
+    
+    void ss(){
+     System.out.println("hello raj");
+     }
+     public static void main(String[]args){
+     cons obj = new cons();
+     obj.ss();
+     }
+     }
+    
