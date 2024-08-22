213a44bced76339b9f221bdfdd8d2a0752457474
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2024-08-22 12:03:04 +0530

    Create method.java

---
+++ method.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class method{
+       void sarath(){
+       System.out.println("hello sarath");
+       }
+       int add(int a, int b)
+       {
+       return a+b;
+       }
+ public static void main(String[]args){
+ method obj= new method();
+ obj.sarath();
+ int result =obj.add(5,10);
+ System.out.println(result);
+ }
+ }
